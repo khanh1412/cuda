e59c75ce6a43555a1f5687c0980f189aa2a1b53e
Nguyen Ngoc Khanh <khanh@localhost.localdomain>
2018-11-06 11:43:51 +0800

    tsp

--- examples/TSP.cu
+++ examples/TSP.cu
@@ -1,6 +1,6 @@
 #include<random>
 #include<iostream>
-
+#include<stdio.h>
 __global__ void TSP(int *perm, float *result, float *arr, int size, int total_threads)
 {
 	int block_id = blockIdx.x;
@@ -8,30 +8,21 @@ __global__ void TSP(int *perm, float *result, float *arr, int size, int total_th
 	int threads = blockDim.x;
 	int id = thread_id + block_id * threads;
 	if (id >= total_threads) return;
-	//copy data to shared memory
-	extern __shared__ float s_arr[];
-	for (int i=thread_id; i<total_threads; i+= threads)
-	{
-		s_arr[i] = arr[i];
-	}
-
-	__syncthreads();
 	//permutation
 	int *pos = perm + id*size;
 	//traversal
 	float cost = 0;
 	int last, curr;
 	
-	for (int i=0; i<size; i++)
-	{
+	for (int i=0; i<size-1; i++)
+	{	
 		last = pos[i];
 		curr = pos[i+1];
-		cost += s_arr[last*size + curr];
+		cost += arr[last*size + curr];
 	}
 	last = pos[size-1];
 	curr = pos[0];
-	cost += s_arr[last*size + curr];
-
+	cost += arr[last*size + curr];
 	result[id] = cost;
 }
 
@@ -68,7 +59,7 @@ void heapPermutation(int a[], int size, int n)
 		}
 	}
 }
-void matrix_randomizer(float density, float *arr, int size)
+void matrix_randomizer(float *arr, int size)
 {
 	std::random_device rd;
         std::uniform_real_distribution<float> dist(0, 1);
@@ -78,20 +69,15 @@ void matrix_randomizer(float density, float *arr, int size)
 
 	for (int i=0; i<size*size; i++)
 	{
-		arr[i] = 0.0;
-		if (dist(prng) < density)
-		{
-			arr[i] = dist(prng);
-		}
+		arr[i] = dist(prng);
 	}
 }
 int main()
 {
-	int size = 10;
-	float density = 0.5;
+	int size = 4;
 
 	float *arr = new float[size*size];
-	matrix_randomizer(density, arr, size);
+	matrix_randomizer(arr, size);
 
 	int total_threads = 1;
 	for (int i=2; i<=size; i++)
@@ -128,7 +114,7 @@ int main()
 	dim3 BlocksPerGrid(HmBlocks, 1, 1);
 	dim3 ThreadsPerBlock(MaxThreadsPerBlock,1,1);
 
-	TSP<<<BlocksPerGrid, ThreadsPerBlock, bytes>>>(d_perm, d_result, d_arr, size, total_threads);
+	TSP<<<BlocksPerGrid, ThreadsPerBlock>>>(d_perm, d_result, d_arr, size, total_threads);
 
 
 	float *result = new float[total_threads];
@@ -150,7 +136,17 @@ int main()
 			count++;
 		}
 		std::cout<<"]";
-		std::cout<<result[i]>>std::endl;
+		std::cout<<result[i]<<std::endl;
+	}
+
+	std::cout<<"GRAPH"<<std::endl;
+	for (int i=0; i<size; i++)
+	{
+		for (int j=0; j<size; j++)
+		{
+			std::cout<<arr[i*size + j]<<" ";
+		}
+		std::cout<<std::endl;
 	}
 
 	delete perm;
